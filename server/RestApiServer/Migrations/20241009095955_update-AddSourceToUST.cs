b783f0a19178f764f6e248d397b968764294f7d3
Iain Marais <91893235+iainmarais@users.noreply.github.com>
2024-10-09 12:00:30 +0200

    Update DbContext.

---
+++ server/RestApiServer/Migrations/20241009095955_update-AddSourceToUST.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestApiServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateAddSourceToUST : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SessionToken",
+                table: "UserSessionTokens",
+                type: "varchar(8000)",
+                maxLength: 8000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Source",
+                table: "UserSessionTokens",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RefreshToken",
+                table: "UserRefreshTokens",
+                type: "varchar(8000)",
+                maxLength: 8000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Source",
+                table: "UserSessionTokens");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SessionToken",
+                table: "UserSessionTokens",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(8000)",
+                oldMaxLength: 8000)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RefreshToken",
+                table: "UserRefreshTokens",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(8000)",
+                oldMaxLength: 8000)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
