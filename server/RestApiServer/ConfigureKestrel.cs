9385d6128dfc69a3109c49512b2f9b6449b965b2
Iain Marais <91893235+iainmarais@users.noreply.github.com>
2025-03-04 11:59:25 +0200

    Alter kestrel configuration for cross platform usage.

--- server/RestApiServer/ConfigureKestrel.cs
+++ server/RestApiServer/ConfigureKestrel.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using RestApiServer.Common.Config;
 using Serilog;
 
@@ -15,6 +16,11 @@ namespace RestApiServer
                 var serverListenLocalhostIp = ConfigurationLoader.GetConfigValue(EnvironmentVariable.ServerListenLocalhostIp);
                 var httpPort = ConfigurationLoader.GetConfigValueAsInt(EnvironmentVariable.ServerHttpPort);
                 var httpsPort = ConfigurationLoader.GetConfigValueAsInt(EnvironmentVariable.ServerHttpsPort);   
+
+                string certFilePath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                string certFileName = "localhost-dev-server.pfx";
+
+                Log.Information($"User home path: {certFilePath}");
                 try
                 {
                     if(args.Length > 0)
@@ -22,17 +28,17 @@ namespace RestApiServer
                         if(args.Contains("host"))
                         {
                             options.Listen(IPAddress.Parse(serverListenPrimaryIp), httpPort);
-                            options.Listen(IPAddress.Parse(serverListenPrimaryIp), httpsPort, o => o.UseHttps("C:\\Users\\Iain\\localhost-dev-server.pfx", "C3r3$123"));
+                            options.Listen(IPAddress.Parse(serverListenPrimaryIp), httpsPort, o => o.UseHttps($"{certFileName}/{certFileName}", "C3r3$123"));
                         }
                         if(args.Contains("dev"))
                         {
                             options.Listen(IPAddress.Parse(serverListenLocalhostIp), httpPort);
-                            options.Listen(IPAddress.Parse(serverListenLocalhostIp), httpsPort, o => o.UseHttps("C:\\Users\\Iain\\localhost-dev-server.pfx", "C3r3$123"));                                
+                            options.Listen(IPAddress.Parse(serverListenLocalhostIp), httpsPort, o => o.UseHttps($"{certFileName}/{certFileName}", "C3r3$123"));                                
                         }
                         else
                         {
                             options.ListenAnyIP(httpPort); //HTTP port       
-                            options.ListenAnyIP(httpsPort, o => o.UseHttps("C:\\Users\\Iain\\localhost-dev-server.pfx", "C3r3$123")); //HTTPS port
+                            options.ListenAnyIP(httpsPort, o => o.UseHttps($"{certFileName}/{certFileName}", "C3r3$123")); //HTTPS port
                         }
                     }                
                 }
@@ -40,7 +46,12 @@ namespace RestApiServer
                 {
                     Log.Error("Could not bind socket, falling back to listen all.", ex);
                     options.Listen(IPAddress.Parse("0.0.0.0"), httpPort); //HTTP port       
-                    options.Listen(IPAddress.Parse("0.0.0.0"), httpsPort, o => o.UseHttps("C:\\Users\\Iain\\localhost-dev-server.pfx", "C3r3$123")); //HTTPS port
+                    options.Listen(IPAddress.Parse("0.0.0.0"), httpsPort, o => o.UseHttps($"{certFileName}/{certFileName}", "C3r3$123")); //HTTPS port
+                }
+                catch (Exception ex)
+                {
+                    Log.Error($"Could not set up HTTPS: {ex.Message}\nFalling back to HTTP only.");
+                    options.Listen(IPAddress.Parse("0.0.0.0"), httpPort);
                 }
             });
         }
